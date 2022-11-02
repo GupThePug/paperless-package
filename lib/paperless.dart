c677ad4b4e451d62924e217477a2ad4cbba7dc11
Eber Jesus Aguilera <61065627+GupThePug@users.noreply.github.com>
2022-11-01 21:53:29 -0600

    debugging

--- lib/paperless.dart
+++ lib/paperless.dart
@@ -82,6 +82,7 @@ class FlutterPaperless extends ConsumerWidget {
       data: (data) => _buildBody(data),
       error: (e, s) {
         if (notifyErrors != null) notifyErrors!(e, s);
+        print(notifyErrors);
         return const CustomMessageWidget(
           title: "Error",
           subTitle: "Error al cargar el formulario, contacte a soporte.",

4d4cbfae6852a7a152471254c2de743e9c329936
Eber Jesus Aguilera <eber25aglera@gmail.com>
2022-11-04 02:06:23 -0600

    debugging part 2

--- lib/widgets/form_widget.dart
+++ lib/widgets/form_widget.dart
@@ -59,10 +59,9 @@ class _FormWidgetPageState extends ConsumerState<FormWidgetPage> {
       data: (form) => createForm(form, context),
       error: (e, s) {
         if (widget.notifyErrors != null) widget.notifyErrors!(e, s);
-        print(widget.notifyErrors);
         return const CustomMessageWidget(
           title: "Error",
-          subTitle: "Error al cargar el formulario, contacte a soporte.",
+          subTitle: "Error al cargar el formulario, contacte a soporte. (--DEBUGGER--) ",
           icon: Icon(Icons.error),
         );
       },
